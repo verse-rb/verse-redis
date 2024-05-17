25b8318885f17dcf94736c29b3c399e72d8a2d6d
Yacine <anykeyh@gmail.com>
2024-05-17 14:30:54 +0700

    add cron task specs

--- lib/verse/periodic/cron_task.rb
+++ lib/verse/periodic/cron_task.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require "parse-cron"
+
 module Verse
   module Periodic
     # CronTask is a task that runs at a specific time, specified by a cron
     # expression.
     class CronTask < Task
       def initialize(name, manager, cron, per_service: true, &block)
-        @work = work
         @per_service = per_service
+
         @cron_rule = CronParser.new(cron)
         @at = @cron_rule.next(Time.now)
 
