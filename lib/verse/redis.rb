a13bb54fe740966a00d6d8efc9cd62f2c6c350d9
Yacine Petitprez <anykeyh@gmail.com>
2025-06-18 14:48:34 +0700

    add distributed counter implementation

--- lib/verse/redis.rb
+++ lib/verse/redis.rb
@@ -6,6 +6,7 @@ require_relative "redis/plugin"
 require_relative "redis/stream/event_manager"
 require_relative "redis/kv_store"
 require_relative "redis/lock"
+require_relative "redis/counter"
 
 require_relative "./ext"
 
